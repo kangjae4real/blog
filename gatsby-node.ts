85254eb2a083c83eed3bc13799ab81d53ad2fe01
Kangjae Choi <awpe89@gmail.com>
2024-07-06 03:28:45 +0900

    chore: base setup

---
+++ gatsby-node.ts
@@ -0,0 +1,15 @@
+import path from "path";
+import { NodePluginArgs } from "gatsby";
+
+type OnCreateWebpackConfigType = (params: NodePluginArgs) => void;
+
+export const onCreateWebpackConfig: OnCreateWebpackConfigType = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@/components": path.resolve(__dirname, "src/components"),
+        "@/lib/utils": path.resolve(__dirname, "src/lib/utils"),
+      },
+    },
+  });
+};
