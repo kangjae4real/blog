12786bc66052f86e44af3dfdf57225bc8fca7de5
Kangjae Choi <awpe89@gmail.com>
2024-07-22 16:54:48 +0900

    refactor: fix /resume, /portfolio page content

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { HeadFC, PageProps } from "gatsby";
 import PageLayout from "@/components/layouts/page-layout";
 import SEO from "@/components/seo";
+import { Large, P } from "@/components/typography";
 
 const PortfolioPage: React.FC<PageProps> = ({ location }) => {
-  return <PageLayout location={location}>Portfolio.</PageLayout>;
+  return (
+    <PageLayout location={location}>
+      <Large>열심히 작성중입니다!</Large>
+      <P className="[&:not(:first-child)]:mt-0">그때 만나요~</P>
+    </PageLayout>
+  );
 };
 
 export default PortfolioPage;
