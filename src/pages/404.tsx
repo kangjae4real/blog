95cb35e7be769a5d55aabe03f8cfd2d4464e37a9
Kangjae Choi <awpe89@gmail.com>
2024-07-22 17:10:13 +0900

    feat: add NotFound component in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { HeadFC, PageProps } from "gatsby";
 import PageLayout from "@/components/layouts/page-layout";
 import SEO from "@/components/seo";
+import NotFound from "@/components/not-found";
 
 const NotFoundPage: React.FC<PageProps> = () => {
-  return <PageLayout>Not Found.</PageLayout>;
+  return (
+    <PageLayout>
+      <NotFound />
+    </PageLayout>
+  );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <SEO />;
+export const Head: HeadFC = () => <SEO titleSuffix="Not found" />;
