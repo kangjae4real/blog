7b76883eeb343cd0784e3dfb94d55c27ad4a9656
Kangjae Choi <awpe89@gmail.com>
2024-08-02 17:27:15 +0900

    feat: add post comment feature

---
+++ src/components/comment.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Giscus, { Repo } from "@giscus/react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+import { H4 } from "@/components/typography";
+
+const commentVariants = cva("mt-6");
+
+interface CommentProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "children">,
+    VariantProps<typeof commentVariants> {}
+
+const Comment: React.FC<CommentProps> = ({ className, ...props }) => {
+  if (
+    !process.env.GATSBY_REPO ||
+    !process.env.GATSBY_REPO_ID ||
+    !process.env.GATSBY_CATEGORY ||
+    !process.env.GATSBY_CATEGORY_ID
+  ) {
+    return (
+      <div className="mt-6 flex w-full justify-center">
+        <H4 className="font-bold">Load environment variable failed.</H4>
+      </div>
+    );
+  }
+
+  return (
+    <div className={cn(commentVariants(), className)} {...props}>
+      <Giscus
+        repo={process.env.GATSBY_REPO as Repo}
+        repoId={process.env.GATSBY_REPO_ID}
+        category={process.env.GATSBY_CATEGORY}
+        categoryId={process.env.GATSBY_CATEGORY_ID}
+        mapping="url"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="light"
+        lang="ko"
+        loading="lazy"
+      />
+    </div>
+  );
+};
+
+export default Comment;
